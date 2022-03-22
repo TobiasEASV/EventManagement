fb3786a8e4dfac659cda086f44b424d4a4bbd2ee
Theut94 <Mikkeltheut@gmail.com>
2022-03-22 09:13:03 +0100

    Added Customer + getter/setter for properties

---
+++ src/be/Customer.java
@@ -0,0 +1,45 @@
+package be;
+
+public class Customer {
+    private String customerEmail;
+
+
+
+
+
+    private String customerName;
+    private int id;
+
+
+
+    public Customer(String customerEmail, String customerName)
+    {
+        this.customerEmail = customerEmail;
+        this.customerName = customerName;
+        this.id = id;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
