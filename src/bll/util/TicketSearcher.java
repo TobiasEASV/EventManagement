5ff5e6f08609f1252740a4fb6533378a14f3be9c
JanWohlgehagen <jan.wohlgehagen@gmail.com>
2022-03-22 13:26:32 +0100

    Created ticketListModel, ISearcher and TicketSaercher

---
+++ src/bll/util/TicketSearcher.java
@@ -0,0 +1,47 @@
+package bll.util;
+
+import be.Ticket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketSearcher implements Isearcher{
+
+    private List<Ticket> searchResult = new ArrayList<>();
+
+    @Override
+    public List<Ticket> search(List<Ticket> searchBase, String query) {
+        searchResult.clear();
+
+        for (Ticket ticket: searchBase) {
+            if(compareToCustomerName(ticket, query) || compareToCustomerEmail(ticket, query))
+            {
+                searchResult.add(ticket);
+            }
+        }
+
+        return searchResult;
+    }
+
+    /**
+     * If isTitleOn is true, it checks if the movie's name field contains the query and returns true if so.
+     * @param ticket a Ticket object
+     * @param query a search query
+     * @return
+     */
+    @Override
+    public boolean compareToCustomerName(Ticket ticket, String query) {
+       return ticket.getCustomerNameProperty().get().toLowerCase().contains(query.toLowerCase());
+    }
+
+    /**
+     * If isTitleOn is true, it checks if the IMDBRating field is higher than the query and returns true if so.
+     * @param ticket a Ticket object
+     * @param query a search query
+     * @return
+     */
+    @Override
+    public boolean compareToCustomerEmail(Ticket ticket, String query) {
+        return ticket.getCustomerEmailProperty().get().toLowerCase().contains(query.toLowerCase());
+    }
+}
--- src/gui/model/TicketListModel.java
+++ src/gui/model/TicketListModel.java
@@ -2,10 +2,14 @@ package gui.model;
 
 import be.Ticket;
 import bll.TicketManager;
+import bll.util.Isearcher;
+import bll.util.TicketSearcher;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class TicketListModel {
@@ -13,17 +17,14 @@ public class TicketListModel {
     private static TicketListModel single_instance = null;
     private TicketManager ticketManager;
     private ObservableList<Ticket> ticketList;
+    private List<Ticket> ticketCache = new ArrayList<>();
+    private Isearcher ticketSearcher;
 
     private TicketListModel() throws IOException {
         ticketManager = new TicketManager();
-        //ticketList = ticketManager.getAllTicketToObservable();
-        //mock
-        ticketList = FXCollections.observableArrayList();
-        ticketList.add(new Ticket());
-        ticketList.add(new Ticket());
-        ticketList.add(new Ticket());
-        ticketList.add(new Ticket());
-
+        ticketList = FXCollections.observableArrayList(ticketManager.getAllTicketToObservable().stream().map(ticket -> new Ticket()).toList());
+        ticketCache.addAll(ticketList);
+        ticketSearcher = new TicketSearcher();
     }
 
     public static TicketListModel getInstance() throws IOException {
@@ -33,7 +34,30 @@ public class TicketListModel {
         return single_instance;
     }
 
-    public ObservableList<Ticket> getAllTickets(){
+    public ObservableList<Ticket> getTicketList(){
         return ticketList;
+    }
+
+    public void addTicketToList(Ticket ticket){
+        ticketList.add(ticketManager.createTicket(ticket));
+    }
+
+    public void deleteTicketFromList(Ticket ticket){
+        ticketList.remove(ticket);
+        ticketManager.deleteTicket(ticket);
+    }
+
+    public void updateTicket(Ticket ticket){
+        ticketManager.updateTicket(ticket);
+    }
+
+    public void searchTicket(String query){
+        ticketList.clear();
+        if (query.isBlank()) {
+            ticketList.addAll(ticketCache);
+        } else {
+            ticketList.addAll(ticketSearcher.search(ticketList, query));
+        }
+
     }
 }
