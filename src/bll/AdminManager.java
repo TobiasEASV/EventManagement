b791c98a485ea97380eef1f860335a8d6e44c55e
Tobias Rasmussen <tobi9782@easv365.dk>
2022-04-03 16:58:20 +0200

    add alert message

--- src/bll/AdminManager.java
+++ src/bll/AdminManager.java
@@ -2,8 +2,8 @@ package bll;
 
 import be.Admin;
 import bll.interfaces.IAdminManager;
-import dal.interfaces.IDBAdminDAO;
 import dal.interfaces.IDatabaseFacade;
+import javafx.scene.control.Alert;
 
 public class AdminManager implements IAdminManager {
 
@@ -15,6 +15,16 @@ public class AdminManager implements IAdminManager {
 
     @Override
     public Admin checkCredential(String userName, String password) {
-        return databaseFacade.checkCredentialForAdmin(userName, password);
+        Admin admin = databaseFacade.checkCredentialForAdmin(userName, password);
+        if(admin != null ){
+            return admin;
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setContentText("username or password is wrong");
+            alert.showAndWait();
+        }
+    return null;
     }
+
 }
--- src/bll/EventCoordinatorManager.java
+++ src/bll/EventCoordinatorManager.java
@@ -3,6 +3,7 @@ package bll;
 import be.EventCoordinator;
 import bll.interfaces.IEventCoordinatorManager;
 import dal.interfaces.IDatabaseFacade;
+import javafx.scene.control.Alert;
 
 import java.util.List;
 
@@ -16,7 +17,16 @@ public class EventCoordinatorManager implements IEventCoordinatorManager {
 
     @Override
     public EventCoordinator checkCredential(String userName, String password) {
-        return databaseFacade.checkCredentialForEventCoordinator(userName, password);
+        EventCoordinator eventCoordinator = databaseFacade.checkCredentialForEventCoordinator(userName, password);
+        if(eventCoordinator != null ){
+            return eventCoordinator;
+        }else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setContentText("username or password is wrong");
+            alert.showAndWait();
+        }
+        return null;
     }
 
     @Override
